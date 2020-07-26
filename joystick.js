f19b48e56c380ffbd7b96dcc4b800174f4d7ceec
subnetpie <subnetpie@hotmail.com>
2020-07-26 13:02:34 -0700

    Update joystick.js

--- joystick.js
+++ joystick.js
@@ -59,8 +59,7 @@ export class Joystick
     }
 
     get_axis_value(axis) {
-        if(axis=="axis0") return 128;
-        if(!this.gamepad) return 64;
+        if(!this.gamepad) return Math.floor((Math.random() * 255));;
         let val = Math.floor((this.gamepad.axes[ps4[axis]] * 128) + 128);
         if(val < 0) val = 0;
         if(val > 255) val = 255;
