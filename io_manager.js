e112b95ac0d92495da6e7e0c4480ac0fdef6e3f7
subnetpie <subnetpie@hotmail.com>
2020-08-11 04:01:46 -0700

    Update io_manager.js

--- io_manager.js
+++ io_manager.js
@@ -92,7 +92,7 @@ export class IOManager
     ////////////////////////////////////////////
     read(addr) {
 
-        this._joystick.cycles++;
+       // this._joystick.cycles++;
 
         if((addr & 0xf000) != 0xc000) return undefined; // default read
 
