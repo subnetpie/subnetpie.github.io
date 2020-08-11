1fc8f66b37568eec31f0799c44cf6127dea3c9ca
subnetpie <subnetpie@hotmail.com>
2020-08-11 13:19:03 -0700

    Update io_manager.js

--- io_manager.js
+++ io_manager.js
@@ -90,9 +90,9 @@ export class IOManager
     }
 
     ////////////////////////////////////////////
-    read(addr) {
+    read(addr) 
 
-       // this._joystick.cycles++;
+        joyValues.delta = (joyValues.cycles - joyValues.trigger);
 
         if((addr & 0xf000) != 0xc000) return undefined; // default read
 
@@ -157,7 +157,7 @@ export class IOManager
                     return this._joystick.axis3;
                 case 0xc070: // trigger paddle read
                     this._joystick.trigger = this._joystick.cycles;
-                    console.log("Trigger: " + this._joystick.trigger);
+                  //  console.log("Trigger: " + this._joystick.trigger);
                     return this._joystick.trigger;
                 case 0xc07e: // iou disable (0: iou is enabled, 0x80: iou is disabled)
                     //console.log("iou disable: " + this._iou_disable);
