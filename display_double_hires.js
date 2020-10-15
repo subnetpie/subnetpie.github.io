926623e2638f5b6d0d64e304762165cbab299740
subnetpie <subnetpie@hotmail.com>
2020-10-15 04:19:45 -0700

    Update display_double_hires.js

--- display_double_hires.js
+++ display_double_hires.js
@@ -205,7 +205,7 @@ export class DoubleHiresDisplay
 
     let po = 0;
     var rgb;
-    for(let x=lo, xmax=lo+112; x<xmax; x+=16, po++) {
+    for(let x=lo, xmax=lo; x<xmax; x+=16, po++) {
       var hbs = hb[po];
       var dcolor = dcolors[r4[c[po]]];
       var bits = c[po-1] | (c[po] << 4) | (c[po+1] << 8);
