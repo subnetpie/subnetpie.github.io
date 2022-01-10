879e2218e9294436a0b47534d42ea98de18bffd7
subnetpie <subnetpie@hotmail.com>
2022-01-09 21:04:46 -0800

    Update io_manager.js

--- appleii/io_manager.js
+++ appleii/io_manager.js
@@ -465,7 +465,7 @@ export class IOManager
 
         if(this._text_mode) {
             // text mode
-            //console.log("enabling text mode: " + (is_page2 ? "page2" : "page1"));
+            alert("enabling text mode: " + (is_page2 ? "page2" : "page1"));
             this._display_text.set_active_page(is_page2 ? 2 : 1);
         } else {
             // graphics modes
