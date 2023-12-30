eb8f4926e87e83461eb07e664dbe11e667f4495f
subnetpie <subnetpie@hotmail.com>
2023-12-29 19:00:17 -0800

    Update display_text.js

--- appleii/display_text.js
+++ appleii/display_text.js
@@ -104,9 +104,9 @@ export class TextDisplay
                     data[p+1] = this._bg;
                     data[p+2] = this._bb;
                     
-                    data[p+2256] = data[p+2260] = this._frl;
-                    data[p+2257] = data[p+2261] = this._fgl;
-                    data[p+2258] = data[p+2262] = this._fbl;
+                    data[p+2256] = data[p+2260] = this._br;
+                    data[p+2257] = data[p+2261] = this._bg;
+                    data[p+2258] = data[p+2262] = this._bb;
                 } else {
                     data[p]    = this._fr;
                     data[p+1]  = this._fg;
