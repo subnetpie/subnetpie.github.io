0a02c8054702a0073eedc173cd37f083852cea03
subnetpie <subnetpie@hotmail.com>
2023-12-30 13:17:35 -0800

    Update display_text.js

--- appleii/display_text.js
+++ appleii/display_text.js
@@ -101,9 +101,9 @@ export class TextDisplay
                 const p = x + y;
                 if(cp & 0x01) {
                     
-                    data[p+0]  = this._br;
-                    data[p+1]  = this._bg;
-                    data[p+2]  = this._bb;
+                    data[p+0]  = data[p+3]  = this._br;
+                    data[p+1]  = data[p+4]  = this._bg;
+                    data[p+2]  = data[p+5]  = this._bb;
                     
                     data[p+2256]  = this._br;
                     data[p+2257]  = this._bg;
