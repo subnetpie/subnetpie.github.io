6f7464b8899e7f26c9f2436aef2524e0d43db6af
subnetpie <subnetpie@hotmail.com>
2023-12-30 21:54:37 -0800

    Update display_text.js

--- appleii/display_text.js
+++ appleii/display_text.js
@@ -135,9 +135,9 @@ export class TextDisplay
                     
                 } else {
                     
-                    data[p+2256]  = this._fr;
-                    data[p+2257]  = this._fg;
-                    data[p+2258]  = this._fb;
+                    //data[p+2256]  = this._fr;
+                    //data[p+2257]  = this._fg;
+                    //data[p+2258]  = this._fb;
                     
                     //var nextOff = off + 564 * 4;
         
@@ -145,9 +145,9 @@ export class TextDisplay
                     //data[x+nextOff+1] = c1;
                     //data[x+nextOff+2] = c2;
         
-                    //data[p+0] = this._frl;
-                    //data[p+1] = this._fgl;
-                    //data[p+2] = this._fb1;
+                    data[p+0] = this._frl;
+                    data[p+1] = this._fgl;
+                    data[p+2] = this._fb1;
                     
                 }
                 cp >>= 1;
