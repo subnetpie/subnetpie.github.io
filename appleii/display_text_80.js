0609eb16d53f7a322c4135e92c736a02da7c86d8
subnetpie <subnetpie@hotmail.com>
2024-01-01 07:56:03 -0800

    Update display_text_80.js

--- appleii/display_text_80.js
+++ appleii/display_text_80.js
@@ -9,7 +9,7 @@
 
 import {rom_342_0265_a} from "https://subnetpie.github.io/appleii/rom/342-0265-a.js";
 
-export class TextDisplay80
+export class TextDisplay
 {
     constructor(memory, canvas) {
         this._font_rom = rom_342_0265_a;
@@ -39,7 +39,6 @@ export class TextDisplay80
     get fore() {
         return (this._fr << 16) | (this._fg << 8) | this._fb;
     };
-    
     set fore(rgb) {
         this._fr = (rgb >> 16) & 0xff;
         this._fg = (rgb >> 8) & 0xff;
@@ -63,7 +62,6 @@ export class TextDisplay80
     get hscan() {
         return this._hscan;
     };
-    
     set hscan(val) {
         this._hscan = (val != 0);
         this._frl = this._hscan ? this._fr>>1 : this._fr;
@@ -81,61 +79,49 @@ export class TextDisplay80
         const col = (addr & 0x7f) % 40;  // column: 0-39
         const row = (((addr - col) >> 2) & 0x18) | ((addr >> 7) & 0x07);
         const id = (addr < 0x0800) ? this._id1 : this._id2;
-        
-        this.draw_char80(id, row, col, val);
+        this.draw_char40(id, row, col, val);
     }
 
     // draw 14x16 char
-    draw_char80(id, row, col, char) {
+    draw_char40(id, row, col, char) {
         if((row > 23) || (col > 39)) return;
-        
+
         const ox = (col * 14) + 2;
         const oy = (row * 16) + 4;
         const lo = (ox + oy * 564) * 4;
         const data = id.data;
 
-        
-            // 7x8 font
-            let csl = char * 8;
-            // 64 * 564 = 36096,  8 * 564 = 4512
-            for(let y=0; y<36096; y+=4512) {
-                let cp = this._font_rom[csl++];
-                // 7 * 8 = 56
-                for(let x=lo, xmax=lo+56; x<xmax; x+=8) {
-                    const p = x + y;
-                    if(cp & 0x01) {
-                    
-                        data[p+0]  = this._br;
-                        data[p+1]  = this._bg;
-                        data[p+2]  = this._bb;
-                    
-                        data[p+2256]  = this._br;
-                        data[p+2257]  = this._bg;
-                        data[p+2258]  = this._bb;
-                    
-                    } else {
-                        
-                        data[p+0] = this._frl;
-                        data[p+1] = this._fgl;
-                        data[p+2] = this._fb1;
-                    
-                        data[p+2256]  = this._fr;
-                        data[p+2257]  = this._fg;
-                        data[p+2258]  = this._fb;
-
-                    }
-                    cp >>= 1;
+        // 7x8 font
+        let csl = char * 8;
+        // 64 * 564 = 36096,  8 * 564 = 4512
+        for(let y=0; y<36096; y+=4512) {
+            let cp = this._font_rom[csl++];
+            // 7 * 8 = 56
+            for(let x=lo, xmax=lo+56; x<xmax; x+=8) {
+                const p = x + y;
+                if(cp & 0x01) {
+                    data[p]   = data[p+4] = data[p+2256] = data[p+2260] = this._br;
+                    data[p+1] = data[p+5] = data[p+2257] = data[p+2261] = this._bg;
+                    data[p+2] = data[p+6] = data[p+2258] = data[p+2262] = this._bb;
+                } else {
+                    data[p]    = data[p+4]  = this._fr;
+                    data[p+1]  = data[p+5]  = this._fg;
+                    data[p+2]  = data[p+6]  = this._fb;
+                    data[p+2256] = data[p+2260] = this._frl;
+                    data[p+2257] = data[p+2261] = this._fgl;
+                    data[p+2258] = data[p+2262] = this._fbl;
                 }
+                cp >>= 1;
             }
-            if(id == this._id) this._context.putImageData(this._id, 0, 0, ox, oy, 14, 16);
-            alert(id+" - "+this.id);
-        
+        }
+
+        if(id == this._id) this._context.putImageData(this._id, 0, 0, ox, oy, 14, 16);
     }
 
     refresh() {
         if(this._id == this._id1) {
             this._id = undefined; // suspend rendering
-            for(let a=0x0400; a<0x0800; a++) this.draw_text(a, this._mem.read(a+2256));
+            for(let a=0x0400; a<0x0800; a++) this.draw_text(a, this._mem.read(a));
             this._id = this._id1;
             this._context.putImageData(this._id, 0, 0);
         }
