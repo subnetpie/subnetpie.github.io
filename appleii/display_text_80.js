130d2540c04ed3770903e6ec7b2101a805cd6a34
subnetpie <subnetpie@hotmail.com>
2024-01-02 00:54:39 -0800

    Update display_text_80.js

--- appleii/display_text_80.js
+++ appleii/display_text_80.js
@@ -122,7 +122,7 @@ export class TextDisplay80
         }
         else if(this._id == this._id2) {
             this._id = undefined; // suspend rendering
-            for(let a=0x0800; a<0x0c00; a++) this.draw_text(a, this._mem.read(a));
+            for(let a=0x0400; a<0x0800; a++) this.draw_text(a, this._mem.read(a));
             this._id = this._id2;
         }
         this._context.putImageData(this._id, 0, 0);
@@ -141,7 +141,7 @@ export class TextDisplay80
             // select page 2
             if(!this._page2_init) {
                 this._id = undefined; // suspend rendering
-                for(let a=0x0800; a<0x0c00; a++) this.draw_text(a, this._mem.read(a));
+                for(let a=0x0400; a<0x0800; a++) this.draw_text(a, this._mem.read(a));
                 this._page2_init = true;
             }
             this._id = this._id2;
