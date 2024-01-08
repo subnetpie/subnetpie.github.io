d45ad9a59922c7c91c08caa09f98cb82123588b5
subnetpie <subnetpie@hotmail.com>
2024-01-07 19:08:41 -0800

    Update io_manager.js

--- appleii/io_manager.js
+++ appleii/io_manager.js
@@ -436,8 +436,8 @@ export class IOManager
             // : text page 80
    
             if(!this._mem.dms_80store) {
-                if( (((addr & 0xfc00) == 0x0400) && !this._mem.dms_80store) || 
-                    (((addr & 0xfc00) == 0x0800) && this._mem.dms_page2 && !this._mem.dms_80store) ) {
+                if( ((addr & 0xfc00) == 0x0400) || 
+                    ((addr & 0xfc00) == 0x0800) ) {
                     this._display_text_80.draw_text(addr, val);
                 }
             } else {
