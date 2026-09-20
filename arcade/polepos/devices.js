66819c326ab778625f2c05f4a5bc9b5bb46b8b05
subnetpie <subnetpie@hotmail.com>
2026-09-20 10:40:52 -0700

    Move Namco 53XX out of Pole Position devices

--- arcade/polepos/devices.js
+++ arcade/polepos/devices.js
@@ -1,4 +1,3 @@
-import { MB8843 } from '../../chips/MB88xx.js';
 
 // Small event queue in the board's 24.576 MHz master-clock domain.
 export class BoardScheduler {
@@ -23,23 +22,4 @@ export class BoardScheduler {
 }
 
 // MAME 0.289 namco53.cpp: MB8843, O latch, K mode and four R inputs.
-export class Namco53XX {
-  constructor() {
-    this.mcu = new MB8843();
-    this.portO = 0; this.resetLine = 1;
-    this.mcu.readK = () => 0;
-    this.mcu.writeO = value => { this.portO = value & 255; };
-  }
-  loadROM(data) {
-    if (data.length !== 0x400) throw new Error('53XX needs a 1024-byte program ROM');
-    this.mcu.loadROM(data);
-  }
-  read() { return this.portO; }
-  chipSelect(state) { this.mcu.setIRQ(state); }
-  setResetLine(level) {
-    const next = level ? 1 : 0;
-    if (next !== this.resetLine && !next) this.mcu.reset();
-    this.resetLine = next;
-  }
-  isReset() { return !this.resetLine; }
-}
+
