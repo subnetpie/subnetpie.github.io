d0a70f983f320fa865eaabb7c73306df546baf32
subnetpie <subnetpie@hotmail.com>
2026-09-22 23:52:03 -0700

    Correct Dig Dug background tile bit orientation

--- arcade/digdug/script.js
+++ arcade/digdug/script.js
@@ -465,14 +465,13 @@ class DigDug {
   }
 
   bgPixel(code, x, y) {
-    // MAME charlayout_2bpp: plane offsets {0,4},
-    // X offsets {64..67, 0..3}, Y offsets {0,8,...56}.
-    const bitBase = (code & 0xff) * 128 +
-                    (x < 4 ? 64 + x : x - 4) +
-                    y * 8;
-    const p0 = (this.bgGfxRom[bitBase >> 3] >> (bitBase & 7)) & 1;
+    // Dig Dug gfx3 uses MAME charlayout_2bpp:
+    // X offsets {64,65,66,67, 0,1,2,3}, planes {0,4}.
+    const xOffset = x < 4 ? 64 + x : x - 4;
+    const bitBase = (code & 0xff) * 128 + xOffset + y * 8;
+    const p0 = (this.bgGfxRom[bitBase >> 3] >> (7 - (bitBase & 7))) & 1;
     const p1Bit = bitBase + 4;
-    const p1 = (this.bgGfxRom[p1Bit >> 3] >> (p1Bit & 7)) & 1;
+    const p1 = (this.bgGfxRom[p1Bit >> 3] >> (7 - (p1Bit & 7))) & 1;
     return p0 | (p1 << 1);
   }
 
